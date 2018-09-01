797d53f67e627768dfe12b53dc0cd93605cafcee
mikol <mikol1980@gmail.com>
2018-09-01 12:10:10 +0200

    [#2.1]

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TextInput, Button } from 'react-native';
 import { Constants } from 'expo';
 
 
@@ -7,11 +7,15 @@ export default class App extends React.Component {
   constructor() {
     super();
      this.state = {
+      title: '',
+      content: '',
       items: [
-        { title: `Title 1`, content: `Note 1`},
-        { title: `Title 2`, content: `Note 2`},
-        { title: `Title 3`, content: `Note 3`},
-        { title: `Title 4`, content: `Note 4`},
+        { id: 1, title: `Title 1`, content: `Note 1`, done: false},
+        { id: 2, title: `Title 2`, content: `Note 2`, done: false},
+        { id: 3, title: `Title 3`, content: `Note 3`, done: true},
+        { id: 4, title: `Title 4`, content: `Note 4`, done: false},
+        { id: 5, title: `Title 5`, content: `Note 5`, done: true},
+        { id: 6, title: `Title 6`, content: `Note 6`, done: false},
 
       ],
     }
@@ -28,11 +32,46 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <TextInput
+         placeholder="Title"
+         returnKeyType="done"
+         style={styles.input}
+         onChangeText={this.titleChanged}
+        />
+        <TextInput
+          placeholder="Content"
+          multiline = {true}
+          numberOfLines = {4}
+          style={styles.input}
+          onChangeText={this.contentChanged}
+          />
+          <Button
+            title="Add note"
+            onPress={this.addNote}
+          />
         <FlatList data={this.state.items} renderItem={this.renderItem} />
       </View>
       
     );
   }
+
+  titleChanged = text =>
+    this.setState(state => ({
+      title: text,
+    }));
+
+  contentChanged = text =>
+    this.setState(state => ({
+      content: text,
+    }));
+
+  addNote = () => this.setState(state => ({
+    items: state.items.concat({
+      title: state.title,
+      content: state.content,
+      done: false,
+    })
+  }))
 }
 
 const styles = StyleSheet.create({
@@ -54,5 +93,8 @@ const styles = StyleSheet.create({
   },
   content: {
     marginBottom: 10,
+  },
+  input: {
+    width: 100,
   },
 });
