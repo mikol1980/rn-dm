e4414789c2ec8245d7b8af6810ec70f2d09ad7a3
mikol <mikol1980@gmail.com>
2018-09-01 13:58:46 +0200

    [#3.1]

--- App.js
+++ App.js
@@ -2,68 +2,153 @@ import React from 'react';
 import { StyleSheet, Text, View, FlatList, TextInput, Button, AsyncStorage, TouchableOpacity } from 'react-native';
 import { Constants } from 'expo';
 import { MaterialIcons } from '@expo/vector-icons'
+import { StackNavigator } from 'react-navigation';
 
 const noteItems = 'devmeeting:text';
 
-export default class App extends React.Component {
-  constructor() {
-    super();
-     this.state = {
-      title: '',
-      content: '',
-      items: [
-        { index: 1, title: `Title 1`, content: `Note 1`, done: false},
-        { index: 2, title: `Title 2`, content: `Note 2`, done: false},
-        { index: 3, title: `Title 3`, content: `Note 3`, done: true},
-        { index: 4, title: `Title 4`, content: `Note 4`, done: false},
-        { index: 5, title: `Title 5`, content: `Note 5`, done: true},
-        { index: 6, title: `Title 6`, content: `Note 6`, done: false},
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Home',
+  };
 
-      ],
-    }
-  }
+  state = {
+    items: [
+      { id: 1, title: `Title 1`, content: `Note 1`, done: false},
+      { id: 2, title: `Title 2`, content: `Note 2`, done: false},
+      { id: 3, title: `Title 3`, content: `Note 3`, done: true},
+      { id: 4, title: `Title 4`, content: `Note 4`, done: false},
+      { id: 5, title: `Title 5`, content: `Note 5`, done: true},
+      { id: 6, title: `Title 6`, content: `Note 6`, done: false},
 
-  componentWillMount() {
-    // AsyncStorage.getItem(noteItems).then(items => this.setState({ items }));
-  }
-    
-
-  renderItem = ({item}) => (
-    <View style={styles.item}>
-      <Text style={styles.title}>{item.title}</Text>
-      <Text style={styles.content}>{item.content}</Text>
-      <TouchableOpacity onPress={() => this.deleteItem(item.index)}>
-        <MaterialIcons name="delete" size={16} color="red"/>
-      </TouchableOpacity>
-      <MaterialIcons name="edit" size={16} color="red" />
-    </View>
-  );
+    ],
+  };
 
   render() {
     return (
-      <View style={styles.container}>
-        <TextInput
-         placeholder="Title"
-         returnKeyType="done"
-         style={styles.input}
-         onChangeText={this.titleChanged}
-        />
-        <TextInput
-          placeholder="Content"
-          multiline = {true}
-          numberOfLines = {4}
-          style={styles.input}
-          onChangeText={this.contentChanged}
-          />
+      <View>
+        <Text>Home</Text>
+        {this.state.items.map(item => (
           <Button
-            title="Add note"
-            onPress={this.addNote}
+            key={item.id}
+            title={`Show note ${item.id}`}
+            onPress={() => this.props.navigation.navigate('Item', item)}
           />
-        <FlatList data={this.state.items} renderItem={this.renderItem} />
+        ))}
       </View>
-      
     );
   }
+}
+
+renderItem = ({item}) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{item.title}</Text>
+    <Text style={styles.content}>{item.content}</Text>
+    <TouchableOpacity onPress={() => this.deleteItem(item.index)}>
+      <MaterialIcons name="delete" size={16} color="red"/>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => this.updateItem(item.index)}>
+      <MaterialIcons name="edit" size={16} color="red" />
+    </TouchableOpacity>
+  </View>
+);
+
+const ItemScreen = ({ navigation }) => (
+  <View>
+    <Text style={styles.title}>{navigation.getParam('title')}</Text>
+    <Text style={styles.content}>{navigation.getParam('content')}</Text>
+    <TouchableOpacity onPress={() => this.deleteItem(navigation.state.params.id)}>
+      <MaterialIcons name="delete" size={16} color="red"/>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => this.updateItem(navigation.state.params.id)}>
+      <MaterialIcons name="edit" size={16} color="red" />
+    </TouchableOpacity>
+    <Button title="Go back" onPress={() => navigation.goBack()} />
+  </View>
+);
+ItemScreen.navigationOptions = ({ navigation }) => ({title: `Item #${navigation.getParam('id')}`})
+
+const Navigator = StackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    AddItem: {
+      scree: AddItemScreen,
+    },
+    Item: {
+      screen: ItemScreen,
+    },
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+
+export default class App extends React.Component {
+  render() {
+    return <Navigator />;
+  }
+  // constructor() {
+  //   super();
+  //    this.state = {
+  //     title: '',
+  //     content: '',
+  //     items: [
+  //       { index: 1, title: `Title 1`, content: `Note 1`, done: false},
+  //       { index: 2, title: `Title 2`, content: `Note 2`, done: false},
+  //       { index: 3, title: `Title 3`, content: `Note 3`, done: true},
+  //       { index: 4, title: `Title 4`, content: `Note 4`, done: false},
+  //       { index: 5, title: `Title 5`, content: `Note 5`, done: true},
+  //       { index: 6, title: `Title 6`, content: `Note 6`, done: false},
+
+  //     ],
+  //   }
+  // }
+
+  // componentWillMount() {
+  //   // AsyncStorage.getItem(noteItems).then(items => this.setState({ items }));
+  // }
+    
+
+  // renderItem = ({item}) => (
+  //   <View style={styles.item}>
+  //     <Text style={styles.title}>{item.title}</Text>
+  //     <Text style={styles.content}>{item.content}</Text>
+  //     <TouchableOpacity onPress={() => this.deleteItem(item.index)}>
+  //       <MaterialIcons name="delete" size={16} color="red"/>
+  //     </TouchableOpacity>
+  //     <TouchableOpacity onPress={() => this.updateItem(item.index)}>
+  //       <MaterialIcons name="edit" size={16} color="red" />
+  //     </TouchableOpacity>
+  //   </View>
+  // );
+
+  // render() {
+  //   return (
+  //     <View style={styles.container}>
+  //       <TextInput
+  //        placeholder="Title"
+  //        returnKeyType="done"
+  //        style={styles.input}
+  //        onChangeText={this.titleChanged}
+  //       />
+  //       <TextInput
+  //         placeholder="Content"
+  //         multiline = {true}
+  //         numberOfLines = {4}
+  //         style={styles.input}
+  //         onChangeText={this.contentChanged}
+  //         />
+  //         <Button
+  //           title="Add note"
+  //           onPress={this.addNote}
+  //         />
+  //       <FlatList data={this.state.items} renderItem={this.renderItem} />
+  //     </View>
+      
+  //   );
+  // }
 
   titleChanged = text =>
     this.setState(state => ({
