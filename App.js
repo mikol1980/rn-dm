6721ec24e68e44df87cbd3ca93bfe9159ada02d9
mikol <mikol1980@gmail.com>
2018-09-01 11:19:45 +0200

    [#1.3]

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { Constants } from 'expo';
+
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+     this.state = {
+      items: [
+        { title: `Title 1`, content: `Note 1`},
+        { title: `Title 2`, content: `Note 2`},
+        { title: `Title 3`, content: `Note 3`},
+        { title: `Title 4`, content: `Note 4`},
+
+      ],
+    }
+  }
+    
+
+  renderItem = ({item}) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{item.title}</Text>
+      <Text style={styles.content}>{item.content}</Text>
+    </View>
+  );
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <FlatList data={this.state.items} renderItem={this.renderItem} />
       </View>
+      
     );
   }
 }
@@ -16,8 +38,21 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 0,
+    margin: 0,
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+  },
+  item: {
+    paddingHorizontal: 10,
+  },
+  title: {
+    fontWeight: 'bold',
+    marginVertical: 5,
+  },
+  content: {
+    marginBottom: 10,
   },
 });
